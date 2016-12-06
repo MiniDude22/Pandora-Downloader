4e55b336502a8d00cfc371ee9faf29a8546c7110
MiniDude22 <minidude22.gamer@gmail.com>
2016-12-05 19:54:00 -0700

    Server now makes playlist files for each station

--- flask_server.py
+++ flask_server.py
@@ -6,31 +6,43 @@ from flask import Flask, request, jsonify
 
 from mutagen.mp4 import MP4, MP4Cover
 
-# Modify this line to change where the songs are saved
+# =============================================================================
+# Config
+# =============================================================================
+
+# Modify These lines to change where the songs are saved
+base_path = os.sep.join(( os.path.expanduser("~"), 'Music', 'Pandora' ))
+
 # Possible Variables - Mix and match as desired
 # {station} {artist} {album} {title}
 save_template = os.sep.join((
-    os.path.expanduser("~"),
-    'Music',
-    'Pandora',
     "{station}",
     "{artist}",
     # "{album} - {title}.m4a"
     "{title}.m4a"
 )).encode('utf-8')
 
+# =============================================================================
+
 app = Flask( "Pandora Downloader Server" )
 
 @app.route( '/download', methods=['POST'] )
 def pandoraDownloader():
 
+    playlist_path = os.sep.join((
+        base_path,
+        re.sub(u'[<>\*:\\/\"|?]', '', request.form['station'].encode('utf-8') ) + ".m3u"
+    ))
+
     # Build the song's path - remove any invalid characters
-    song_path = save_template.format(
+    relative_song_path = save_template.format(
         station = re.sub(u'[<>\*:\\/\"|?]', '', request.form['station'].encode('utf-8') ),
         artist  = re.sub(u'[<>\*:\\/\"|?]', '', request.form['artist' ].encode('utf-8') ),
         album   = re.sub(u'[<>\*:\\/\"|?]', '', request.form['album'  ].encode('utf-8') ),
         title   = re.sub(u'[<>\*:\\/\"|?]', '', request.form['title'  ].encode('utf-8') )
     ).decode('utf-8')
+
+    song_path = os.sep.join(( base_path, relative_song_path ))
 
     # check to see if the file has been downloaded before!
     if os.path.exists( song_path ):
@@ -38,6 +50,7 @@ def pandoraDownloader():
         return jsonify( status = 'alreadyDownloaded' )
 
     else:
+
         print( 'Downloading "' + request.form['title'].encode('utf-8') + '"' )
 
         # Create the directories if they don't exist
@@ -46,12 +59,15 @@ def pandoraDownloader():
         # Download the song
         urllib.urlretrieve( request.form['url'], song_path )
 
+        # Open the song with mutagen so we can tag it and put the album art in it
         song = MP4( song_path )
 
+        # Set the tags
         song['\xa9nam'] = [ request.form['title'].encode('utf-8')  ]
         song['\xa9ART'] = [ request.form['artist'].encode('utf-8') ]
         song['\xa9alb'] = [ request.form['album'].encode('utf-8')  ]
 
+        # Download the album art and put it in the file
         albumArtRequest = urllib.urlopen( request.form['albumArt'] )
         albumArt = MP4Cover( albumArtRequest.read() )
         albumArtRequest.close()
@@ -60,6 +76,10 @@ def pandoraDownloader():
 
         song.save()
 
+        # Append the song in the playlist
+        with open( playlist_path, 'a+' ) as playlist:
+            playlist.write( relative_song_path + "\n" )
+        
         print( 'Download Complete!' )
         return jsonify( status = 'success' )
 
